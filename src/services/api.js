3683edc44d712af4b0c6afe2e4b0f0729350c109
priyanshu <parmanand.visko@gmail.com>
2026-06-03 11:47:30 +0530

    add live backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://trinetra-backend-sxge.onrender.com/api' })
+const api = axios.create({ baseURL: 'https://api.trinetraglobalholidays.com/api' })
+// const api = axios.create({ baseURL: 'https://trinetra-backend-sxge.onrender.com/api' })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('admin_token')
