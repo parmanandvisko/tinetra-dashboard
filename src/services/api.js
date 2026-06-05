42fe54ec4bb3532a415be468e1de7cb977aa516e
priyanshu <parmanand.visko@gmail.com>
2026-06-05 18:16:08 +0530

    last changes api changes

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,7 @@ api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem('admin_token')
+      localStorage.removeItem('admin_token') 
       window.location.href = '/login'
     }
     return Promise.reject(err)
